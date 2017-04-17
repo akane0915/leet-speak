1ff1397d4777658e90f33f7df7d90cfacefe9067
Asia Kane and James Higgins <asia@>
2017-04-17 13:53:45 -0700

    add other else statments so s is swithced with z, but only when it does not start the word

--- leet_speak/lib/leet_speak.rb
+++ leet_speak/lib/leet_speak.rb
@@ -2,15 +2,24 @@ class String
   define_method(:leet_speak) do
     letters = self.split("")
     leet_letters = []
-    letters.each() do |letter|
+    letters.each_with_index() do |letter,index|
       if letter.===("e")|letter.===("E")
         leet_letters.push("3")
       elsif letter.===("o")|letter.===("O")
         leet_letters.push("0")
       elsif letter.===("I")
         leet_letters.push("1")
+      elsif letter.===("s")|letter.===("S")
+        if letters[index-1].===(" ")
+          leet_letters.push(letter)
+        else
+          leet_letters.push("z")
+        end
       else
         leet_letters.push(letter)
+      end
+      if leet_letters[0].===("z")
+        leet_letters[0].replace("S")
       end
     end
     leet_letters
--- leet_speak/spec/leet_speak_spec.rb
+++ leet_speak/spec/leet_speak_spec.rb
@@ -7,12 +7,15 @@ describe('String#leet_speak') do
   # end
 
   it('replaces every e with a 3') do
-    expect(("test").leet_speak()).to(eq(["t", "3", "s", "t"]))
+    expect(("tet").leet_speak()).to(eq(["t", "3", "t"]))
   end
   it('replaces every o with a 0') do
-    expect(("tost").leet_speak()).to(eq(["t", "0", "s", "t"]))
+    expect(("tot").leet_speak()).to(eq(["t", "0", "t"]))
   end
   it('replaces every I with a 1') do
-    expect(("Isi").leet_speak()).to(eq(["1", "s", "i"]))
+    expect(("Iti").leet_speak()).to(eq(["1", "t", "i"]))
+  end
+  it('replaces any s that is not the first letter of a word with z') do
+    expect(("Isi second tests").leet_speak()).to(eq(["1", "z", "i", " ", "s", "3", "c", "0", "n", "d", " ", "t", "3", "z", "t", "z"]))
   end
 end
